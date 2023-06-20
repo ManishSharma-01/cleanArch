9653e73f0e4a775d056ec12a102aaf6ba54a40e3
ManishSharma-01 <27.maanish@gmail.com>
2023-06-20 08:21:56 +0545

    Add

---
+++ lib/features/batch/domain/repository/batch_repository.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../../core/failure/failure.dart';
+// import '../../data/repository/batch_local_repo_impl.dart';
+import '../../data/repository/batch_remote_repo_provider.dart';
+import '../entity/batch_entity.dart';
+
+final batchRepositoryProvider = Provider<IBatchRepository>((ref) {
+  // Return Local repository implementation
+  // For internet connectivity we will check later
+  return ref.read(batchRemoteRepoProvider);
+  // return ref.read(batchLocalRepoProvider);
+});
+
+abstract class IBatchRepository {
+  Future<Either<Failure, List<BatchEntity>>> getAllBatches();
+  Future<Either<Failure, bool>> addBatch(BatchEntity batch);
+}
